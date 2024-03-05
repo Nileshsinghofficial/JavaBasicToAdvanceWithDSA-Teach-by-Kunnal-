651292a379670d06a11bf7ca47afc17a813d919c
nileshsinghofficial <nigamsingh888@gmail.com>
2024-03-05 21:51:48 +0530

    Primitives

--- src/com/nilesh/Main.java
+++ src/com/nilesh/Main.java
@@ -1,6 +1,12 @@
 package com.nilesh;
+
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.println("HelloNileshSingh");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
---
+++ src/com/nilesh/Primitives.java
@@ -0,0 +1,19 @@
+package com.nilesh;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumber = 456755556.5667;
+        long largeInteger = 347854334456778L;
+        boolean check = false;
+
+
+        // String is not primitives
+        String name = "Nilesh";
+
+
+        System.out.println();
+    }
+}
