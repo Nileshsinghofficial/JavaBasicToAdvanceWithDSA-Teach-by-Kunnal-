cc9543a3e7e4a31ed3ef6f4dd749bd8abe893fa3
nileshsinghofficial <nigamsingh888@gmail.com>
2024-03-05 22:51:20 +0530

    How to Print Millions Number

---
+++ src/com/nilesh/Inputs.java
@@ -0,0 +1,15 @@
+package com.nilesh;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter Your Number: ");
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll number is " + rollno);
+
+        int a = 203_000_000;
+        System.out.println(a);
+    }
+}
