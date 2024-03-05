570f75e41c573688b15f18373ef7eb51064dba02
nileshsinghofficial <nigamsingh888@gmail.com>
2024-03-05 23:00:50 +0530

    use Next() and NextLine() Function

--- src/com/nilesh/Inputs.java
+++ src/com/nilesh/Inputs.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
+          Scanner input = new Scanner(System.in);
 //        System.out.println("Enter Your Number: ");
 //        int rollno = input.nextInt();
 //        System.out.println("Your roll number is " + rollno);
 
-        int a = 203_000_000;
-        System.out.println(a);
+//        int a = 203_000_000;
+//        System.out.println(a);
+        System.out.println("If you want only first sentence");
+        String name = input.next();
+        System.out.println(name);
+        System.out.println("If you Want print full sentence then do its");
+        String names = input.nextLine();
+        System.out.println(names);
+
     }
 }
