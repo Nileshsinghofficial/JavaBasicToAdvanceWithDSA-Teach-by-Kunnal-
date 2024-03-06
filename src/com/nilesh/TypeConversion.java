908e6e1289aed6827884109d78617efe86dc97af
nileshsinghofficial <nigamsingh888@gmail.com>
2024-03-06 23:56:00 +0530

    Automatic type promotion in expression

--- src/com/nilesh/TypeConversion.java
+++ src/com/nilesh/TypeConversion.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class TypeConversion {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
 //        Type Conversion
-        int num = (int) input.nextFloat();
-        System.out.println("Print this input as a Integer: " + num);
+//        int num = (int) input.nextFloat();
+//        System.out.println("Print this input as a Integer: " + num);
 
 //        Automatic type promotion in expressions
-        int a = 257;
-        byte b = (byte) (a);
+        int a = 600;
+        byte b = (byte)(a);
+//        value(600) % 256 = what come remainder ? (88) => 256 * 2 + 88 = 600
         System.out.println(b);
+
     }
 }
